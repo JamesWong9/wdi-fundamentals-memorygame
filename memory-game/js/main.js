0262f9cbe8a6cb687664eb4236eb897cc4d7fe8c
James Wong <kinchong.wong@gmail.com>
2020-02-17 12:17:13 +0800

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,7 +1,15 @@
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
+const cards = ["queen", "queen", "king", "king"];
+const cardsInPlay = [];
+const cardOne = cards[0];
+cardsInPlay.push('cardOne'); 
+console.log("User flipped queen")
+const cardTwo = cards[2];
+cardsInPlay.push('cardTwo');
+console.log("User flipped king")
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+cardsInPlay.length === 2;
+
+if (cardOne === cardTwo) {alert("You found a match!")
+} else{alert("Sorry, try again.")
+}
+
