f09dd72e80f69a6f7f0f1c03cc0c89d59b7378b9
James Wong <kinchong.wong@gmail.com>
2020-02-18 12:18:52 +0800

    create game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,4 +1,26 @@
-const cards = ["queen", "queen", "king", "king"];
+const cards = [
+{
+rank: "queen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png",
+},
+{
+rank: "queen",
+suit: "diamond",
+cardImage: "images/queen-of-diamonds.png",
+},
+{
+rank: "king",
+suit: "hearts",
+cardImage: "images/king-of-hearts.png",
+},
+{
+rank: "king",
+suit: "diamonds",
+cardImage: "images/king-of-diamonds.png",
+}
+];
+
 const cardsInPlay = [];
 
 function checkForMatch(){
@@ -13,8 +35,10 @@ checkForMatch();
 
 function flipCard(cardId) {
 cards[cardId]
-console.log("User flipped " + cards[cardId])
-cardsInPlay.push(cards[cardId])
+console.log("User flipped " + cards[cardId].rank)
+console.log("User flipped " + cards[cardId].cardImage)
+console.log("User flipped " + cards[cardId].suit)
+cardsInPlay.push(cards[cardId].rank)
 cardsInPlay.length === 2;
 }
 
